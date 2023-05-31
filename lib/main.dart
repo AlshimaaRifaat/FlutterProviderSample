672eac0cec3197b120e4f1e66b084a427cc05cb0
Alshimaa Refaat <Alshimaa.Refaat-External@alfuttaim.com>
2023-05-31 15:18:05 +0300

    Navigate to the right screen

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyAppState extends State<MyApp> {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home:  HomeScreen(),
+      home:  LoginScreen(),
     );
   }
 }
